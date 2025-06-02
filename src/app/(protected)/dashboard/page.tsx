3bfa9d4ff345439dac283fbc1d1c44c6d55c054d
MatheusZamo <19matheushenrique03@gmail.com>
2025-06-02 19:27:51 -0300

    feat: add top specialties list

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -20,6 +20,7 @@ import AppointmentsChart from "./components/appoitments-chart";
 import { DatePicker } from "./components/date-picker";
 import StatsCards from "./components/stats-cards";
 import TopDoctors from "./components/top-doctors";
+import TopSpecialties from "./components/top-specialties";
 
 interface DashboardPageProps {
   searchParams: Promise<{
@@ -54,6 +55,7 @@ const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
     [totalPatients],
     [totalDoctors],
     topDoctorsData,
+    topSpecialtiesData,
   ] = await Promise.all([
     db
       .select({
@@ -112,6 +114,52 @@ const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
       .groupBy(doctorsTable.id)
       .orderBy(desc(count(appointmentsTable.id)))
       .limit(10),
+    db
+      .select({
+        specialty: doctorsTable.specialty,
+        appointments: count(appointmentsTable.id),
+      })
+      .from(appointmentsTable)
+      .innerJoin(doctorsTable, eq(appointmentsTable.doctorId, doctorsTable.id))
+      .where(
+        and(
+          eq(appointmentsTable.clinicId, session.user.clinic.id),
+          gte(appointmentsTable.date, new Date(from)),
+          lte(appointmentsTable.date, new Date(to)),
+        ),
+      )
+      .groupBy(doctorsTable.specialty)
+      .orderBy(desc(count(appointmentsTable.id))),
+    db.query.appointmentsTable.findMany({
+      where: and(
+        eq(appointmentsTable.clinicId, session.user.clinic.id),
+        gte(appointmentsTable.date, new Date()),
+        lte(appointmentsTable.date, new Date()),
+      ),
+      with: {
+        patient: true,
+        doctor: true,
+      },
+    }),
+    // db
+    //   .select({
+    //     date: sql<string>`DATE(${appointmentsTable.date})`.as("date"),
+    //     appointments: count(appointmentsTable.id),
+    //     revenue:
+    //       sql<number>`COALESCE(SUM(${appointmentsTable.appointmentPriceInCents}), 0)`.as(
+    //         "revenue",
+    //       ),
+    //   })
+    //   .from(appointmentsTable)
+    //   .where(
+    //     and(
+    //       eq(appointmentsTable.clinicId, session.user.clinic.id),
+    //       gte(appointmentsTable.date, chartStartDate),
+    //       lte(appointmentsTable.date, chartEndDate),
+    //     ),
+    //   )
+    //   .groupBy(sql`DATE(${appointmentsTable.date})`)
+    //   .orderBy(sql`DATE(${appointmentsTable.date})`),
   ]);
 
   const chartStartDate = dayjs().subtract(10, "days").startOf("day").toDate();
@@ -160,6 +208,9 @@ const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
         <div className="grid grid-cols-[2.25fr_1fr] gap-4">
           <AppointmentsChart dailyAppointmentsData={dailyAppointmentsData} />
           <TopDoctors doctors={topDoctorsData} />
+        </div>
+        <div className="grid grid-cols-[2.25fr_1fr] gap-4">
+          <TopSpecialties topSpecialties={topSpecialtiesData} />
         </div>
       </PageContent>
     </PageContainer>
